4134f0725619e1740018ec6739f6753834159b94
Ciprian Dragoste <xyboox@gmail.com>
2017-01-17 11:34:57 +0200

    2017.01.17

---
+++ APP/www/js/initFirebase.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+
+	var config = {
+	    apiKey: "AIzaSyB1k-SdNg7EOoxIoZaXR7_7nnSomBPj4jU",
+	    authDomain: "golive-dc48d.firebaseapp.com",
+	    databaseURL: "https://golive-dc48d.firebaseio.com",
+	    storageBucket: "golive-dc48d.appspot.com",
+	    messagingSenderId: "337138194018"
+	};
+	firebase.initializeApp(config);
+	console.log('Firebase --> INITED');
+})();
